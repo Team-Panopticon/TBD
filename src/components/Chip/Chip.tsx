8535b49ebc2cb7f6e92313b6a05af592def8f6e5
juyoungpark718 <juyoung7018.park@gmail.com>
2023-02-04 11:52:59 +0900

    feat: UserChip 컴포넌트 추가

---
+++ src/components/Chip/Chip.tsx
@@ -0,0 +1,22 @@
+import React, { CSSProperties, PropsWithChildren } from 'react';
+import { ChipContainer } from './styled';
+
+interface Props {
+  className?: string;
+  style?: CSSProperties;
+  checked?: boolean;
+  focus?: boolean;
+  onClick?: () => void;
+}
+
+export function Chip(props: PropsWithChildren<Props>) {
+  const { className, style, checked, focus, children, onClick } = props;
+
+  return (
+    <div className={className} style={style}>
+      <ChipContainer onClick={onClick} checked={checked} focus={focus}>
+        {children}
+      </ChipContainer>
+    </div>
+  );
+}
---
+++ src/components/Chip/styled.ts
@@ -0,0 +1,33 @@
+import styled, { css } from 'styled-components';
+
+export const ChipContainer = styled.div<{ checked?: boolean; focus?: boolean }>`
+  display: inline-flex;
+  height: 22px;
+  padding: 0 4px;
+  align-items: center;
+  box-shadow: ${({ focus }) => (focus ? `inset 0 0 0 2px #009568` : `none`)};
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  ${(props) => {
+    const {
+      checked,
+      theme: {
+        palette: { primary, secondary },
+      },
+    } = props;
+
+    if (checked) {
+      return css`
+        background-color: ${primary.main};
+        color: white;
+      `;
+    }
+
+    return css`
+      background-color: ${secondary.main};
+    `;
+  }};
+`;
