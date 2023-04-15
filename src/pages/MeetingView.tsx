b14d86100aeb272846186f6d10751575da7f5d85
HojinLee <hojin5633@gmail.com>
2023-04-15 10:56:20 +0900

    feat: voteTable 컴포넌트에 들어가는 data selector 추가
    - voteTable 컴포넌트에 들어가는 data를 만들기 위해 필요한 데이터들의 selector를 voting.ts에 추가
    
    Co-authored-by: juyoungpark718 <juyoungpark718@users.noreply.github.com>
    Co-authored-by: Junsu Shin <junsushin4546@gmail.com>

--- src/pages/MeetingView.tsx
+++ src/pages/MeetingView.tsx
@@ -1,30 +1,39 @@
 import { useEffect, useState } from 'react';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 
-import { getUsers, Users } from '../apis/users';
-import { UserList, UserListData } from '../components/UserList/UserList';
+import { getMeeting } from '../apis/meetings';
+import { GetMeetingResponse } from '../apis/types';
+import { getUsers, GetUsersResponse } from '../apis/users';
+import { UserList } from '../components/UserList/UserList';
+import { VoteTable } from '../components/VoteTable/VoteTable';
 import { currentUserState } from '../stores/currentUser';
+import { userListState, userMapState, voteTableDataListState } from '../stores/voting';
 
 export function MeetingView() {
   const currentUser = useRecoilValue(currentUserState);
   const [isViewMode, setIsViewMode] = useState<boolean>(!!currentUser);
-  const [voteList, setVoteList] = useState<any>({});
-  const [userMap, setUserMap] = useState<Users>({});
+
+  const MEETING_ID = '1';
+
+  const setUserMap = useSetRecoilState<GetUsersResponse>(userMapState);
+  const [meeting, setMeeting] = useState<GetMeetingResponse>();
+
+  const userList = useRecoilValue(userListState);
+  const voteTableDataList = useRecoilValue(voteTableDataListState(meeting));
 
   useEffect(() => {
     (async () => {
       const data = await getUsers(1);
       setUserMap(data);
-    })();
-  }, []);
 
-  const userList = Object.keys(userMap).map<UserListData>((username) => {
-    return {
-      username,
-      checked: false,
-      focused: false,
-    };
-  });
+      const meetingData = await getMeeting(MEETING_ID);
+      setMeeting(meetingData);
+    })();
+  }, [setUserMap]);
+
+  if (!meeting || !voteTableDataList) {
+    return null;
+  }
 
   return (
     <div>
@@ -32,7 +41,7 @@ export function MeetingView() {
       <div>toast message</div>
       <UserList users={userList} />
       {/* <VoteTable data={mockData} headers={['점심', '저녁']} /> */}
-      {/* <VoteTable data={mockData2} headers={['투표 현황']} /> */}
+      <VoteTable data={voteTableDataList} headers={['투표 현황']} />
       {isViewMode ? <div>다시 투표하러 가기</div> : <div>다음에하기 + 투표하기</div>}
     </div>
   );
---
+++ src/stores/voting.ts
@@ -0,0 +1,103 @@
+import dayjs, { Dayjs } from 'dayjs';
+import { atom, selector, selectorFamily } from 'recoil';
+
+import { GetMeetingResponse, ISODateTime } from '../apis/types';
+import { GetUsersResponse } from '../apis/users';
+import { UserListData } from '../components/UserList/UserList';
+import { VoteTableData } from '../components/VoteTable/VoteTable';
+import { MealType } from '../constants/meeting';
+
+// 원본 데이터 있고, 원본데이터를 정제
+// 프론트쓸때 -> 정제해서 보내주고
+// setUserList(VotingManager의 반환값), setVoteTable(VotingMananer의 반환값)
+// 핸들러 -> VotingManager값 바꾸고, 프론트값으로해서 -> useState
+// recoil 쓰는게 정답?
+// [대안] selector로 매니저에서 할 함수들을 작성한다.
+
+/**
+ *  https://github.com/facebookexperimental/Recoil/issues/629
+ *  https://www.typescriptlang.org/docs/handbook/2/mapped-types.html
+ */
+type SelectorMapper<Type> = {
+  [Property in keyof Type]: Type[Property];
+};
+
+interface DateVoting {
+  date: ISODateTime;
+}
+
+interface MealVoting {
+  date: ISODateTime;
+  meal: MealType;
+}
+
+interface UserVoting {
+  date: DateVoting[];
+  meals?: MealVoting[];
+}
+
+export const getVotingCountByDay = (day: Dayjs, userVotings: UserVoting[]) => {
+  return userVotings.reduce((acc, votings) => {
+    const hasVotedForGivenDay = votings.date.some((d) => day.isSame(dayjs(d.date), 'day'));
+
+    if (hasVotedForGivenDay) {
+      return acc + 1;
+    }
+
+    return acc;
+  }, 0);
+};
+
+export const userMapState = atom<GetUsersResponse>({
+  key: 'userMap',
+  default: {},
+});
+
+export const voteTableDataListState = selectorFamily<
+  VoteTableData[] | undefined,
+  SelectorMapper<GetMeetingResponse | undefined>
+>({
+  key: 'voteTableDataList',
+  get:
+    (meeting: GetMeetingResponse | undefined) =>
+    ({ get }) => {
+      const userVotings = get(userVotingsState);
+      const total = userVotings.length;
+
+      return meeting?.dates.map(dayjs).map<VoteTableData>((day) => ({
+        date: day,
+        votings: [
+          {
+            current: getVotingCountByDay(day, userVotings),
+            total,
+            checked: false,
+            focused: false,
+          },
+        ],
+      }));
+    },
+});
+
+export const userVotingsState = selector({
+  key: 'userVotings',
+  get: ({ get }) => {
+    const userMap = get(userMapState);
+
+    return Object.values(userMap).map((user) => user.votings);
+  },
+});
+
+export const userListState = selector({
+  key: 'userList',
+  get: ({ get }) => {
+    const userMap = get(userMapState);
+
+    return Object.values(userMap).map<UserListData>((user) => {
+      return {
+        username: user.name,
+        checked: false,
+        focused: false,
+      };
+    });
+  },
+});
