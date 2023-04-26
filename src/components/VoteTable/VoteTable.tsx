e2ba48bbc17f565a37747ba23a0fdf16ae3cf5de
Junsu Shin <junsushin4546@gmail.com>
2023-04-26 22:42:14 +0900

    feat: 각 투표 Slot에서 렌더링에 필요한 데이터를 직접 수정하도록 리팩토링
    - Table > Row > Slot 으로 분리되는 계층 구조
    - Table의 상위에서 한꺼번에 checked, count 등을 계산할 경우 복잡도가 증가
    - 각 Slot에서 userMap과 currentUserId를 기반으로 직접 계산하도록 변경

--- src/components/VoteTable/VoteTable.tsx
+++ src/components/VoteTable/VoteTable.tsx
@@ -1,8 +1,7 @@
 import { Dayjs } from 'dayjs';
-import { CSSProperties, ReactNode } from 'react';
 
-import { DateVoting, MealVoting } from '../../stores/voting';
-import { UserListVoteData } from '../UserList/UserList';
+import { UserMap } from '../../apis/users';
+import { MealType, MeetingType } from '../../constants/meeting';
 import {
   ContentBox,
   ContentWrapper,
@@ -13,38 +12,33 @@ import {
   VoteTableContainer,
   Wrapper,
 } from './styled';
+import { hasUserVotedForSlot } from './utils';
 
-export interface VoteTableVoting extends UserListVoteData {
-  total: number;
-  current: number;
+export interface SlotClickHandlerProps {
+  date: Dayjs;
+  mealType?: MealType;
 }
 
-export interface VoteTableRowData {
-  date: Dayjs; // 현재 투표 가능한 날짜
-  votings: [VoteTableVoting, VoteTableVoting] | [VoteTableVoting]; // [meal, meal], [date]
+type SlotClickHandler = ({ date, mealType }: SlotClickHandlerProps) => void;
+
+interface VoteTableProps {
+  dates: Dayjs[];
+  meetingType: MeetingType;
+  userMap: UserMap;
+  currentUserId?: string;
+  onSlotClick: SlotClickHandler;
 }
 
-type onClickHandler = (date: Dayjs, checked: boolean, target: VoteTableVoting) => void;
-
-interface Props {
-  className?: string;
-  style?: CSSProperties;
-  rowData: VoteTableRowData[];
-  onClick?: onClickHandler;
-
-  headers: ReactNode[];
-}
-
-// VoteTable의 역할은?
+// VoteTable의 역할
 // 필요한 정보 렌더링: 투표 수 / 총 인원 수, 득표율, 특정 유저가 투표한 칸 highlight
 // 투표 칸의 클릭 정보 전달: Date, MealType, 마지막 클릭된 칸
-// highlighting을 외부에서 수행해서 전달할 것인가, 아니면 VoteTable 내부에서 계산할 것인가?
+export const VoteTable: React.FC<VoteTableProps> = (props) => {
+  const { dates, meetingType, userMap, currentUserId, onSlotClick } = props;
 
-export const VoteTable: React.FC<Props> = (props) => {
-  const { rowData, onClick, style, className, headers } = props;
+  const headers = meetingType === MeetingType.date ? ['날짜'] : ['점심', '저녁'];
 
   return (
-    <VoteTableContainer className={className} style={style}>
+    <VoteTableContainer>
       <Header>
         <HeaderBox>투표 가능 날짜</HeaderBox>
         <Divider />
@@ -53,8 +47,15 @@ export const VoteTable: React.FC<Props> = (props) => {
         ))}
       </Header>
       <ContentWrapper>
-        {rowData.map((item, idx) => (
-          <VoteTableRow onSlotClick={onClick} key={`vote-item-${idx}`} item={item} />
+        {dates.map((date, idx) => (
+          <VoteTableRow
+            key={`vote-item-${idx}`}
+            date={date}
+            meetingType={meetingType}
+            userMap={userMap}
+            currentUserId={currentUserId}
+            onSlotClick={onSlotClick}
+          />
         ))}
       </ContentWrapper>
     </VoteTableContainer>
@@ -63,48 +64,74 @@ export const VoteTable: React.FC<Props> = (props) => {
 
 interface VoteTableRowProps {
   date: Dayjs;
-  votingSlots: (DateVoting | MealVoting)[];
-  onSlotClick?: onClickHandler;
+  meetingType: MeetingType;
+  userMap: UserMap;
+  currentUserId?: string;
+  onSlotClick: SlotClickHandler;
 }
 
 const VoteTableRow: React.FC<VoteTableRowProps> = (props) => {
-  const { date, votingSlots, onSlotClick } = props;
+  const { date, meetingType, userMap, currentUserId, onSlotClick } = props;
 
   return (
     <Wrapper>
       <DateContentBox>{date.format('M/D (dd)')}</DateContentBox>
       <Divider />
-      {votingSlots.map((slot, idx) => {
-        const { current, total, focused, checked } = vote;
-        return <VoteTableSlot votingSlot={slot} votingStatus={} />;
-      })}
+      {meetingType === MeetingType.date && (
+        <VoteTableSlot
+          date={date}
+          userMap={userMap}
+          currentUserId={currentUserId}
+          onSlotClick={onSlotClick}
+        />
+      )}
+      {meetingType === MeetingType.meal &&
+        [MealType.dinner, MealType.lunch].map((mealType) => (
+          <VoteTableSlot
+            key={mealType}
+            date={date}
+            mealType={mealType}
+            userMap={userMap}
+            currentUserId={currentUserId}
+            onSlotClick={onSlotClick}
+          />
+        ))}
     </Wrapper>
   );
 };
 
-interface VotingStatus {
-  currentCount: number;
-  totalCount: number;
-}
-
 interface VoteTableSlotProps {
-  checked: boolean;
-  focused: boolean;
-  votingSlot: DateVoting | MealVoting;
-  votingStatus: VotingStatus;
-  onClick: (newVote: DateVoting | MealVoting) => void;
+  date: Dayjs;
+  mealType?: MealType;
+  userMap: UserMap;
+  currentUserId?: string;
+  onSlotClick: SlotClickHandler;
 }
 
 const VoteTableSlot: React.FC<VoteTableSlotProps> = (props) => {
-  const { checked, focused, votingSlot, votingStatus, onClick } = props;
-  const { currentCount, totalCount } = votingStatus;
-  const currentVotePercentage = ((currentCount / totalCount) * 100).toFixed(0);
+  const { date, mealType, userMap, currentUserId, onSlotClick } = props;
+
+  // TODO: focused 상태 관리
+
+  const currentUser = currentUserId && userMap[currentUserId];
+  const hasCurrentUserVotedForSlot =
+    !!currentUser && hasUserVotedForSlot({ user: currentUser, date, mealType });
+
+  const users = Object.values(userMap);
+  const votedUserCount = users.filter((user) =>
+    hasUserVotedForSlot({ user, date, mealType }),
+  ).length;
+  const totalUserCount = users.length;
+  const votedPercentage = ((votedUserCount / totalUserCount) * 100).toFixed(0);
+
+  const handleClick = () => {
+    onSlotClick({ date, mealType });
+  };
 
   return (
     <ContentBox
-      focus={focused}
-      checked={checked}
-      onClick={() => onClick(votingSlot)}
-    >{`${currentCount}/${totalCount} (${currentVotePercentage}%)`}</ContentBox>
+      checked={hasCurrentUserVotedForSlot}
+      onClick={handleClick}
+    >{`${votedUserCount}/${totalUserCount} (${votedPercentage}%)`}</ContentBox>
   );
 };
