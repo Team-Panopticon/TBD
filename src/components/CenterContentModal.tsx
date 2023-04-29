9a33b50a7b83f725cdc3bd4651d58b0fcfa3374c
HojinLee <hojin5633@gmail.com>
2023-04-29 11:19:04 +0900

    refactor: CenterContentModal 추가, InputPasswordModal의 Modal과 스타일 컴포넌트 적용
    - InputPasswordModal의 Modal과 Modal의 영역을 설정하는 스타일 컴포넌트를
      적용한 CentralContentModal 추가
    
      중앙에 Content 영역이 있는 모달을 재사용 하기 위해 리팩토링

---
+++ src/components/CenterContentModal.tsx
@@ -0,0 +1,43 @@
+import { Modal } from '@mui/material';
+import { PropsWithChildren } from 'react';
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+export const Content = styled.div<{ width: number; height: number }>`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  background-color: white;
+  width: ${({ width }) => `${width}px`};
+  height: ${({ height }) => `${height}px`};
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+interface Props {
+  open: boolean;
+  width: number;
+  height: number;
+}
+
+export function CenterContentModal(props: PropsWithChildren<Props>) {
+  const { open, width, height, children } = props;
+
+  return (
+    <Modal open={open}>
+      <Container>
+        <Content width={width} height={height}>
+          {children}
+        </Content>
+      </Container>
+    </Modal>
+  );
+}
