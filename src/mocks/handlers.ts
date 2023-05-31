8ef151358e3937bf83656e7b6d715494e0215e23
HojinLee <hojin5633@gmail.com>
2023-05-31 21:26:59 +0900

    chore: mock 서버 user -> voting 변경

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,18 +1,18 @@
 import { rest } from 'msw';
 
 import { mockDateTypeMeeting, mockMealTypeMeeting } from './meetingData';
-import { mockDateUserData, mockMealUserData } from './userData';
+import { mockDateVotingsData, mockMealVotingsData } from './votingsData';
 
 const baseURL = import.meta.env.VITE_API_BASE_URL as string;
 export const handlers = [
   rest.get(`${baseURL}meetings/:meetingId/users`, (req, res, ctx) => {
     const meetingId = req.params.meetingId;
     if (meetingId === '1') {
-      return res(ctx.status(200), ctx.json(mockDateUserData));
+      return res(ctx.status(200), ctx.json(mockDateVotingsData));
     }
 
     if (meetingId === '2') {
-      return res(ctx.status(200), ctx.json(mockMealUserData));
+      return res(ctx.status(200), ctx.json(mockMealVotingsData));
     }
 
     return res(ctx.status(404));
