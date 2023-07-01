b200ba33e6bc74d502c526956d6ab996f2581025
juyoungpark718 <juyoung7018.park@gmail.com>
2023-07-01 11:47:40 +0900

    fix: Chip 스타일 원복

--- src/components/Chip/styled.ts
+++ src/components/Chip/styled.ts
@@ -1,8 +1,10 @@
 import { styled } from '@mui/material';
 
 export const ChipContainer = styled('div')<{ checked?: boolean; focus?: boolean }>((props) => ({
-  height: '24px',
-  padding: '6px 8px',
+  display: 'inline-flex',
+  height: '22px',
+  padding: '0 4px',
+  alignItems: 'center',
   boxShadow: `${props.focus ? `inset 0 0 0 2px #009568` : `none`}`,
   borderRadius: '2px',
   fontSize: '12px',
