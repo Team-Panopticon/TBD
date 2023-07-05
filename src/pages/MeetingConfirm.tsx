95ff146cb3b51626792adc431c0920f6d4c39db6
Junsu Shin <junsushin4546@gmail.com>
2023-07-05 21:14:11 +0900

    feat: useMeetingView hook을 이용해서 확정 slot 선택 시 highlight 표시

--- src/pages/MeetingConfirm.tsx
+++ src/pages/MeetingConfirm.tsx
@@ -1,10 +1,20 @@
 import { Button } from '@mui/material';
 import { useQuery } from '@tanstack/react-query';
+import { Dayjs } from 'dayjs';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useSetRecoilState } from 'recoil';
 
 import { getMeeting } from '../apis/meetings';
+import { getVotings, Voting, VotingSlot } from '../apis/votes';
 import { Contents, Footer, Header, HeaderContainer, Page } from '../components/pageLayout';
 import { FullHeightButtonGroup } from '../components/styled';
+import { UserList } from '../components/UserList/UserList';
+import { VoteTable, VoteTableVoting } from '../components/VoteTable/VoteTable';
+import { MeetingType } from '../constants/meeting';
+import { useMeetingView } from '../hooks/useMeetingView';
+import { isSameSlot } from '../hooks/useMeetingVote';
+import { votingsState } from '../stores/voting';
 
 interface MeetingConfirmPathParams {
   meetingId: string;
@@ -21,9 +31,44 @@ export function MeetingConfirm() {
     queryFn: () => getMeeting(meetingId),
   });
 
+  const setVotings = useSetRecoilState<Voting[]>(votingsState);
+  const {
+    handleClickVoteTable: handleVoteTableClickHightlight,
+    userList,
+    voteTableDataList,
+  } = useMeetingView(meeting);
+
+  const [selectedSlot, setSelectedSlot] = useState<VotingSlot>();
+
+  // TODO: Recoil로 비동기 데이터 가져오는 것 대체
+  useEffect(() => {
+    (async () => {
+      if (!meetingId) {
+        return;
+      }
+
+      const data = await getVotings(meetingId);
+      setVotings(data);
+    })();
+  }, [setVotings, meetingId]);
+
   if (isLoading || isError) {
     return null;
   }
+
+  const handleClickVoteTable = (
+    date: Dayjs,
+    checked: boolean,
+    target: VoteTableVoting,
+    slot: VotingSlot,
+  ) => {
+    if (selectedSlot && isSameSlot(selectedSlot, slot)) {
+      setSelectedSlot(undefined);
+    } else {
+      setSelectedSlot(slot);
+    }
+    handleVoteTableClickHightlight(date, checked, target, slot);
+  };
 
   return (
     <Page>
@@ -33,12 +78,12 @@ export function MeetingConfirm() {
         </HeaderContainer>
       </Header>
       <Contents>
-        {/* <UserList users={userList} onClick={handleClickUserList} />
+        <UserList users={userList} />
         <VoteTable
           onClick={handleClickVoteTable}
           data={voteTableDataList}
           headers={meeting.type === MeetingType.date ? ['투표 현황'] : ['점심', '저녁']}
-        /> */}
+        />
       </Contents>
       <Footer>
         <FullHeightButtonGroup
@@ -57,6 +102,7 @@ export function MeetingConfirm() {
           </Button>
           <Button
             color="primary"
+            disabled={!selectedSlot}
             onClick={() => {
               console.log('모임시간 확정');
             }}
