3f8ad62c7443bfb8f7f1cabae1fa8c7354e740ac
Junsu Shin <junsushin4546@gmail.com>
2023-07-22 11:51:45 +0900

    feat: 유저 선택 해제 시 layout shift가 나타나지 않도록 텍스트 높이 고정

--- src/pages/MeetingVote.tsx
+++ src/pages/MeetingVote.tsx
@@ -154,16 +154,18 @@ export function MeetingVote() {
               <FlexVertical alignItems={'center'}>
                 <img height={110} src={WritingHands} alt="" />
               </FlexVertical>
-              {currentUser ? (
-                <Typography variant="h6" fontWeight={400} align="center">
-                  <PrimaryBold className="primary-bold">{currentUser.username}</PrimaryBold>님의
-                  투표를 수정합니다
-                </Typography>
-              ) : (
-                <Typography variant="h6" fontWeight={400} align="center">
-                  투표하신 적이 있으면 참석자 목록에서 아이디를 눌러주세요
-                </Typography>
-              )}
+              <FlexVertical height={64}>
+                {currentUser ? (
+                  <Typography variant="h6" fontWeight={400} align="center">
+                    <PrimaryBold className="primary-bold">{currentUser.username}</PrimaryBold>님의
+                    투표를 수정합니다
+                  </Typography>
+                ) : (
+                  <Typography variant="h6" fontWeight={400} align="center">
+                    투표하신 적이 있으면 참석자 목록에서 아이디를 눌러주세요
+                  </Typography>
+                )}
+              </FlexVertical>
             </FlexVertical>
           </FlexVertical>
         </HeaderContainer>
