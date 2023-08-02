bbb7f16cddde78d8e01a24e0bf52393bd35d99f3
HojinLee <hojin5633@gmail.com>
2023-08-02 21:51:01 +0900

    feat: MeetingModify 페이지 추가
    - 모임 수정용 updateMeeting api 추가
    
    - modifyMeeting 상태는 미사용으로 삭제
    
    - MeetingModfiy 페이지 추가
    
      MeetingCreate 페이지를 기초로 MeetingModify 페이지 추가

--- src/pages/MeetingModify.tsx
+++ src/pages/MeetingModify.tsx
@@ -1,21 +1,85 @@
-import { useRecoilState } from 'recoil';
+import { useQuery } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
+import { useEffect, useMemo, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 
+import { getMeeting, updateMeeting } from '../apis/meetings';
+import { Meeting } from '../apis/types';
 import { Page } from '../components/pageLayout';
+import { MeetingStatus, MeetingType } from '../constants/meeting';
 import useMeetingEdit from '../hooks/useMeetingEdit';
-import { modifyMeetingState } from '../stores/modifyMeeting';
+import { MeetingEditTemplate } from '../templates/MeetingEdit/MeetingEditTemplate';
+
+interface MeetingViewPathParams {
+  meetingId: string;
+}
+
+export const initialState = {
+  id: '',
+  name: '',
+  dates: [],
+  type: MeetingType.date,
+  status: MeetingStatus.inProgress,
+  password: undefined,
+};
 
 export function MeetingModify() {
-  const [meeting, setMeeting] = useRecoilState(modifyMeetingState);
+  const [meeting, setMeeting] = useState<Meeting>(initialState);
+  const { meetingId } = useParams<keyof MeetingViewPathParams>() as MeetingViewPathParams;
   const { getMeetingEditSteps } = useMeetingEdit();
-  const meetingeditSteps = getMeetingEditSteps('modify');
+
+  const {
+    data: initialMeeting,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ['meeting', meetingId],
+    queryFn: () => getMeeting(meetingId),
+  });
+
+  useEffect(() => {
+    if (initialMeeting) {
+      setMeeting(initialMeeting);
+    }
+  }, [initialMeeting, setMeeting]);
+
+  const meetingeditSteps = useMemo(() => {
+    return getMeetingEditSteps('modify');
+  }, [getMeetingEditSteps]);
+
+  const navigate = useNavigate();
+
+  if (!meeting || isLoading || isError) {
+    return;
+  }
+
+  const handleMeetingEditComplete = () => {
+    modifyMeetingAndNavigate();
+  };
+
+  const modifyMeetingAndNavigate = async () => {
+    try {
+      const response = await updateMeeting(meeting);
+      navigate(`/meetings/${response.id}`);
+    } catch (e: unknown) {
+      if (e instanceof AxiosError) {
+        alert(e.message);
+      } else {
+        alert('알수 없는 에러가 발생했습니다');
+      }
+    }
+  };
+
   return (
     <Page>
-      {/* <MeetingEditTemplate
+      <MeetingEditTemplate
+        currentStep={meetingeditSteps.length - 1}
         meeting={meeting}
         meetingEditSteps={meetingeditSteps}
-        step={meetingeditSteps.length - 1}
         onChange={setMeeting}
-      ></MeetingEditTemplate> */}
+        onSubmit={handleMeetingEditComplete}
+        pageType="modify"
+      ></MeetingEditTemplate>
     </Page>
   );
 }
