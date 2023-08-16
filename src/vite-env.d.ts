69c37e92609bdb1710b9ad34f2614ca4ef3ad02f
juyoungpark718 <juyoung7018.park@gmail.com>
2023-08-16 20:38:17 +0900

    feat: 카카오톡 sdk 추가 및 타입 정의

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -7,3 +7,35 @@ declare module '*.svg' {
   const src: string;
   export default src;
 }
+
+type CreateDefaultButtonFeedParams = {
+  container: HTMLElement | string;
+  objectType: 'feed';
+  content: {
+    title: string;
+    description: string;
+    imageUrl: string;
+    /** [내 애플리케이션] > [플랫폼] 에서 등록한 사이트 도메인과 일치해야 함 */
+    link: {
+      mobileWebUrl: string;
+      webUrl: string;
+    };
+  };
+  buttons?: {
+    title: string;
+    link: {
+      mobileWebUrl: string;
+      webUrl: string;
+    };
+  }[];
+};
+
+interface Window {
+  Kakao: {
+    init: (APIKey: string) => void;
+    isInitialized: () => boolean;
+    Share: {
+      createDefaultButton: (params: CreateDefaultButtonFeedParams) => void;
+    };
+  };
+}
