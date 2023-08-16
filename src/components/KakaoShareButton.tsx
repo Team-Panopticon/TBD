81ba69e396c476ee1156eff9691ba1a747dec375
juyoungpark718 <juyoung7018.park@gmail.com>
2023-08-16 21:09:16 +0900

    refactor: ShareButton을 공통 컴포넌트로 빼고 공유버튼에 에러처리 추가

--- src/components/KakaoShareButton.tsx
+++ src/components/KakaoShareButton.tsx
@@ -1,26 +1,17 @@
-import { Avatar, ListItem, ListItemAvatar, ListItemButton, ListItemText } from '@mui/material';
 import React, { useRef } from 'react';
 
 import { useKakaoShare } from '../hooks/useKakaoShare';
+import { ShareButton } from './ShareButton';
 
 export const KakaoShareButton = () => {
   const ref = useRef<HTMLAnchorElement>(null);
 
-  useKakaoShare({
+  const { isError, serviceName } = useKakaoShare({
     ref,
     title: 'XXX 모임 투표',
     description: '모임 투표를 부탁드려요.',
     url: 'https//localhost:5173',
   });
 
-  return (
-    <ListItem disableGutters>
-      <ListItemButton autoFocus ref={ref} href="">
-        <ListItemAvatar>
-          <Avatar>+</Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="카카오톡" />
-      </ListItemButton>
-    </ListItem>
-  );
+  return <ShareButton isError={isError} serviceName={serviceName} ref={ref} />;
 };
--- src/hooks/useKakaoShare.ts
+++ src/hooks/useKakaoShare.ts
@@ -12,29 +12,43 @@ export const useKakaoShare = ({
   description: string;
 }) => {
   const [isInitialized, setInitialized] = useState(false);
+  const [isError, setError] = useState(false);
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
     if (!window.Kakao.isInitialized()) {
-      window.Kakao.init('1bef82833af6e7bc26767e5391f5014b');
-      setInitialized(true);
+      try {
+        window.Kakao.init('1bef82833af6e7bc26767e5391f5014b');
+        setInitialized(true);
+      } catch {
+        setError(true);
+        setLoading(false);
+      }
     }
   }, []);
 
   useEffect(() => {
     if (isInitialized && ref.current) {
-      window.Kakao.Share.createDefaultButton({
-        container: ref.current,
-        objectType: 'feed',
-        content: {
-          title,
-          description,
-          link: {
-            mobileWebUrl: url,
-            webUrl: url,
+      try {
+        window.Kakao.Share.createDefaultButton({
+          container: ref.current,
+          objectType: 'feed',
+          content: {
+            title,
+            description,
+            link: {
+              mobileWebUrl: url,
+              webUrl: url,
+            },
+            imageUrl: '',
           },
-          imageUrl: '',
-        },
-      });
+        });
+      } catch {
+        setError(true);
+        setLoading(false);
+      }
     }
   }, [description, isInitialized, ref, title, url]);
+
+  return { isError, isLoading: !isError && !isInitialized && !isLoading, serviceName: '카카오톡' };
 };
