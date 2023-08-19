3952770b1630483c62d0112b5c4d08a655932ec3
juyoungpark718 <juyoung7018.park@gmail.com>
2023-08-19 11:18:23 +0900

    refactor: env.d.ts에 env 속성의 타입을 선언

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,15 @@
 /// <reference types="vite/client" />
 
+interface ImportMetaEnv {
+  readonly VITE_API_BASE_URL: string;
+  readonly VITE_KAKAOTALK_JS_SDK_KEY: string;
+  readonly VITE_ORIGIN_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
 declare module '*.svg' {
   import React = require('react');
 
