a18b8cc274b03c37e797a77a955de92296c78771
Junsu Shin <junsushin4546@gmail.com>
2023-10-04 21:52:00 +0900

    feat: 모바일 브라우저의 virtual keyboard에서 Done 버튼 클릭 시 다음 step으로 진행

--- src/templates/MeetingEdit/MeetingEditTemplate.tsx
+++ src/templates/MeetingEdit/MeetingEditTemplate.tsx
@@ -14,6 +14,7 @@ import {
   validateMeetingName,
   validateSelectedDates,
 } from '../../stores/createMeeting';
+import { hasTouchScreen } from '../../utils/hasTouchScreen';
 import { MeetingEditStepper } from './MeetingEditStepper';
 import { SelectDates } from './SelectDates';
 import { SelectMeetingType } from './SelectMeetingType';
@@ -80,7 +81,7 @@ export function MeetingEditTemplate<T extends CreateMeetingState | Meeting>({
           meetingEditSteps={meetingEditSteps
             .map((step) => ({
               ...step,
-              component: getMeetingEditContent(step.type, onChange, meeting),
+              component: getMeetingEditContent(step.type, onChange, setStep, meeting),
             }))
             .reverse()}
         ></MeetingEditStepper>
@@ -110,6 +111,7 @@ export function MeetingEditTemplate<T extends CreateMeetingState | Meeting>({
 const getMeetingEditContent = <T extends CreateMeetingState | Meeting>(
   type: IMeetingEditStep['type'],
   setValue: SetterOrUpdater<T>,
+  setStep: SetterOrUpdater<number> | undefined,
   meeting: T,
 ) => {
   switch (type) {
@@ -127,6 +129,12 @@ const getMeetingEditContent = <T extends CreateMeetingState | Meeting>(
           error={isMeetingInValid}
           helperText={helperText}
           value={meeting.name ?? ''}
+          onBlur={() => {
+            // Mobile Browser
+            if (hasTouchScreen) {
+              setStep?.((prev) => prev + 1);
+            }
+          }}
           onChange={(v) => {
             if (v.target.value.length > 30) {
               return;
