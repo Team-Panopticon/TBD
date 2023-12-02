9192bd130718bd28af19f8849e60bc576361637c
juyoungpark718 <juyoung7018.park@gmail.com>
2023-12-02 11:04:04 +0900

    feat: useMeetingViewHeader 커스텀 훅 작성

---
+++ src/hooks/MeetingView/useMeetingViewHeader.ts
@@ -0,0 +1,90 @@
+import { useMutation } from '@tanstack/react-query';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useRecoilState, useRecoilValue } from 'recoil';
+
+import { issuePublicMeetingAdminToken } from '../../apis/meetings';
+import { INPUT_PASSWORD_FINISH_EVENT } from '../../constants/meeting';
+import { adminTokenStateFamily } from '../../stores/adminToken';
+import { currentUserStateFamily } from '../../stores/currentUser';
+import { showVoteSuccessPopupState } from '../../stores/showVoteSuccessPopup';
+import { useMeetingData } from '../useMeetingData';
+import { useProgress } from '../useProgress';
+
+export const useMeetingViewHeader = () => {
+  const { meeting, meetingId } = useMeetingData();
+  const navigate = useNavigate();
+  const [adminToken, setAdminToken] = useRecoilState(adminTokenStateFamily(meetingId));
+  const { show, hide } = useProgress();
+  const [showPasswordModal, setShowPasswordModal] = useState<boolean>(false);
+  const [showVoteSuccessPopup, setShowVoteSuccessPopup] = useRecoilState(showVoteSuccessPopupState);
+  const currentUser = useRecoilValue(currentUserStateFamily(meetingId));
+  const { mutate } = useMutation({
+    mutationFn: async (params: { meetingId: string; destination: string }) =>
+      issuePublicMeetingAdminToken(params.meetingId),
+    onMutate: () => show(),
+    onSuccess: (token, { destination }) => {
+      setAdminToken(token);
+      navigate(destination);
+    },
+    onSettled: () => hide(),
+  });
+
+  // Create a promise that resolves when the user closes the password input modal
+  const openInputPasswordModal = () => {
+    setShowPasswordModal(true);
+
+    const inputPasswordFinishPromise = new Promise((resolve, reject) => {
+      addEventListener(
+        INPUT_PASSWORD_FINISH_EVENT,
+        (event) => resolve((event as CustomEvent).detail),
+        {
+          once: true,
+        },
+      );
+    });
+
+    return inputPasswordFinishPromise;
+  };
+
+  const handlePasswordModalConfirm = () => {
+    dispatchEvent(new CustomEvent(INPUT_PASSWORD_FINISH_EVENT, { detail: true }));
+  };
+
+  const handlePasswordModalCancel = () => {
+    dispatchEvent(new CustomEvent(INPUT_PASSWORD_FINISH_EVENT, { detail: false }));
+    setShowPasswordModal(false);
+  };
+
+  const handleClickSettingsButton = async (destination: string) => {
+    const isLoggedInAsAdmin = adminToken !== undefined;
+    if (isLoggedInAsAdmin) {
+      navigate(destination);
+      return;
+    }
+
+    if (meeting?.adminAccess === 'public') {
+      mutate({ meetingId, destination });
+      return;
+    }
+
+    // Private meeting AND Not yet logged in as admin
+    const isInputPasswordResolved = await openInputPasswordModal();
+
+    if (isInputPasswordResolved) {
+      navigate(destination);
+    }
+  };
+
+  return {
+    currentUser,
+    meeting,
+    meetingId,
+    showPasswordModal,
+    showVoteSuccessPopup,
+    setShowVoteSuccessPopup,
+    handleClickSettingsButton,
+    handlePasswordModalConfirm,
+    handlePasswordModalCancel,
+  };
+};
