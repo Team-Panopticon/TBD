eab6f6d1e28704feebf95c63c6ba70376463f99c
Junsu Shin <junsushin4546@gmail.com>
2023-12-06 22:08:34 +0900

    feat: Welcome 페이지 컨텐츠 영역 상단에 메인 문구와 이미지 추가

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -1,7 +1,9 @@
 import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 import { useNavigate } from 'react-router-dom';
 
-import { Contents, Footer, Header, Page } from '../../components/pageLayout';
+import diningImage from '../../assets/dining.png';
+import { Footer, Header, Page } from '../../components/pageLayout';
 import { FullHeightButtonGroup } from '../../components/styled';
 import { AnimatedTypography } from '../../templates/MeetingEdit/styled';
 import { MainImageContainer, WelcomeContents, WelcomeHeaderContainer } from './styled';
@@ -22,6 +24,22 @@ function Welcome() {
           </AnimatedTypography>
         </WelcomeHeaderContainer>
       </Header>
+      <WelcomeContents>
+        <MainImageContainer>
+          <Typography
+            variant="h4"
+            fontWeight={500}
+            align="center"
+            color={'#FFFFFF'}
+            marginTop={'24px'}
+          >
+            손쉽게 모임시간을
+            <br />
+            정해보세요
+          </Typography>
+          <img src={diningImage} style={{ width: '100%' }} />
+        </MainImageContainer>
+      </WelcomeContents>
       <Footer>
         <FullHeightButtonGroup fullWidth disableElevation variant="contained">
           <Button onClick={handleClick}>{'모임 만들기'}</Button>
---
+++ src/pages/Welcome/styled.tsx
@@ -0,0 +1,23 @@
+import { styled } from '@mui/material/styles';
+
+export const WelcomeHeaderContainer = styled('div')`
+  background-color: ${({ theme }) => theme.palette.primary.main};
+  height: 48px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const MainImageContainer = styled('section')`
+  background-color: ${({ theme }) => theme.palette.primary.main};
+  height: 480px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const WelcomeContents = styled('div')`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
