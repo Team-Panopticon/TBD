38e4a4ce61d64cf666966f1f8a9f7b5c73fbba00
juyoungpark718 <juyoung7018.park@gmail.com>
2023-12-06 21:50:13 +0900

    feat: MeetingVote에서 header 분리

--- src/pages/MeetingVote/MeetingVote.tsx
+++ src/pages/MeetingVote/MeetingVote.tsx
@@ -1,21 +1,17 @@
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
 import { useEffect } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 
-import WritingHands from '../../assets/writing.svg';
 import { Loading } from '../../components/Loading';
-import { Header, HeaderContainer, Page } from '../../components/pageLayout';
-import { FlexVertical } from '../../components/styled';
+import { Page } from '../../components/pageLayout';
 import { useMeeting } from '../../hooks/useMeeting';
 import { useVotings } from '../../hooks/useVotings';
 import { currentUserStateFamily } from '../../stores/currentUser';
 import { currentUserVotingSlotsState } from '../../stores/currentUserVotingSlots';
 import { votingsState as votingRecoilState } from '../../stores/voting';
-import { PrimaryBold } from '../../templates/MeetingView/styled';
 import MeetingVoteContents from '../../templates/MeetingVote/MeetingVoteContents';
 import MeetingVoteFooter from '../../templates/MeetingVote/MeetingVoteFooter';
+import MeetingVoteHeader from '../../templates/MeetingVote/MeetingVoteHeader';
 
 function MeetingVote() {
   const [searchParams] = useSearchParams();
@@ -56,45 +52,7 @@ function MeetingVote() {
 
   return (
     <Page>
-      <Header>
-        <HeaderContainer>
-          <FlexVertical flex={1} alignItems={'center'} gap={1}>
-            <FlexVertical flex={1} gap={1} width={'100%'}>
-              <Box
-                display={'flex'}
-                flexDirection={'row'}
-                justifyContent={'center'}
-                alignItems={'center'}
-                gap={1}
-              >
-                <Typography variant="h5" fontWeight={700}>
-                  {meeting.name}
-                </Typography>
-              </Box>
-              <FlexVertical alignItems={'center'}>
-                <img height={110} src={WritingHands} alt="" />
-              </FlexVertical>
-              <FlexVertical>
-                {currentUser ? (
-                  <Typography variant="h6" fontWeight={400} align="center">
-                    <PrimaryBold className="primary-bold">{currentUser.username}</PrimaryBold>님의
-                    투표를 수정합니다
-                  </Typography>
-                ) : (
-                  <Typography
-                    variant="h6"
-                    fontWeight={400}
-                    style={{ wordBreak: 'keep-all' }}
-                    align="center"
-                  >
-                    투표하신 적이 있으면 참석자 목록에서 아이디를 눌러주세요
-                  </Typography>
-                )}
-              </FlexVertical>
-            </FlexVertical>
-          </FlexVertical>
-        </HeaderContainer>
-      </Header>
+      <MeetingVoteHeader />
       <MeetingVoteContents />
       <MeetingVoteFooter />
     </Page>
