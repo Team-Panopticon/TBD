4b5fbe01ab4385fe474d115c6c16c65f6d7fb167
Junsu Shin <junsushin4546@gmail.com>
2023-12-06 21:07:52 +0900

    feat: Welcome 페이지 레이아웃 및 하단 버튼 추가

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -1,5 +1,20 @@
+import Button from '@mui/material/Button';
+
+import { Contents, Footer, Header, Page } from '../../components/pageLayout';
+import { FullHeightButtonGroup } from '../../components/styled';
+
 function Welcome() {
-  return <div>Welcome</div>;
+  return (
+    <Page>
+      <Header>Header</Header>
+      <Contents>Contents</Contents>
+      <Footer>
+        <FullHeightButtonGroup fullWidth disableElevation variant="contained">
+          <Button>{'모임 만들기'}</Button>
+        </FullHeightButtonGroup>
+      </Footer>
+    </Page>
+  );
 }
 
 export default Welcome;
