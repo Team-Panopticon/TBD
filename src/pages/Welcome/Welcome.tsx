0e01b564baba332830f380c7582b84f05374c514
Junsu Shin <junsushin4546@gmail.com>
2023-12-06 21:09:35 +0900

    feat: 모임 만들기 버튼 클릭 시 모임생성 페이지로 이동

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -1,16 +1,23 @@
 import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
 
 import { Contents, Footer, Header, Page } from '../../components/pageLayout';
 import { FullHeightButtonGroup } from '../../components/styled';
 
 function Welcome() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/meetings/new');
+  };
+
   return (
     <Page>
       <Header>Header</Header>
       <Contents>Contents</Contents>
       <Footer>
         <FullHeightButtonGroup fullWidth disableElevation variant="contained">
-          <Button>{'모임 만들기'}</Button>
+          <Button onClick={handleClick}>{'모임 만들기'}</Button>
         </FullHeightButtonGroup>
       </Footer>
     </Page>
