dd65fe2da21c27044c0176aa34b9a4f268ad3896
juyoungpark718 <juyoung7018.park@gmail.com>
2023-12-16 11:26:07 +0900

    refactor: 이미 useMeetingViewContents 훅에서 이펙트를 부르고 있기 때문에 삭제

--- src/pages/MeetingView/MeetingView.tsx
+++ src/pages/MeetingView/MeetingView.tsx
@@ -3,17 +3,11 @@ import { useResizeDetector } from 'react-resize-detector';
 
 import { ScrollDownFloatingButton } from '../../components/buttons/ScrollDownFloatingButton';
 import { Page } from '../../components/pageLayout';
-import { useMeeting } from '../../hooks/useMeeting';
-import { useVotings } from '../../hooks/useVotings';
 import MeetingViewContents from '../../templates/MeetingView/MeetingViewContents';
 import MeetingViewFooter from '../../templates/MeetingView/MeetingViewFooter';
 import MeetingViewHeader from '../../templates/MeetingView/MeetingViewHeader';
 
 function MeetingView() {
-  const { isFetching: isMeetingFetching } = useMeeting();
-  const { isFetching: isVotingsFetcing } = useVotings();
-  const isFetching = isMeetingFetching && isVotingsFetcing;
-
   const pageElementRef = useRef<HTMLDivElement>(null);
 
   const [hasMoreBottomScroll, setHasMoreBottomScroll] = useState<boolean>(false);
