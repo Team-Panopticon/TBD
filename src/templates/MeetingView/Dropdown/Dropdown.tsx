75e62c64edcb8ca534aaa61b271e1cc09f57c764
saeeng <saeeng.opt@gmail.com>
2023-12-20 20:45:21 +0900

    feat: menu 클릭시 이동

--- src/templates/MeetingView/Dropdown/Dropdown.tsx
+++ src/templates/MeetingView/Dropdown/Dropdown.tsx
@@ -1,6 +1,7 @@
 import Drawer from '@mui/material/Drawer';
 import Typography from '@mui/material/Typography';
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import logo_nobg from '../../../assets/round.png';
 import { Flex } from '../../../components/styled';
@@ -24,62 +25,65 @@ export function Dropdown(props: Props) {
 
   return (
     <div style={{ width: 0 }}>
-      {
-        <Drawer
-          onClose={() => {
-            setShow(false);
-          }}
-          open={show}
-          anchor="right"
+      <Drawer
+        onClose={() => {
+          setShow(false);
+        }}
+        open={show}
+        anchor="right"
+      >
+        <Flex
+          minWidth={200}
+          paddingX={2}
+          paddingTop={2}
+          paddingBottom={1}
+          alignItems={'center'}
+          gap={1}
         >
-          <Flex
-            minWidth={200}
-            paddingX={2}
-            paddingTop={2}
-            paddingBottom={1}
-            alignItems={'center'}
-            gap={1}
-          >
-            <img
-              src={logo_nobg}
-              alt="로고"
-              style={{
-                width: 'auto',
-                height: '28px',
-              }}
-            />
-            <Typography variant="h6" fontWeight={600}>
+          <img
+            src={logo_nobg}
+            alt="로고"
+            style={{
+              width: 'auto',
+              height: '28px',
+            }}
+          />
+          <Typography variant="h6" fontWeight={600}>
+            <NavLink to="/" style={{ textDecoration: 'none' }}>
+              {/* Todo: logo로 변경 */}
               Motoo.day
-            </Typography>
-          </Flex>
-          <Flex
-            paddingX={1}
-            paddingBottom={1}
-            alignItems="flex-start"
-            flexDirection={'column'}
-            justifyContent={'space-between'}
-          >
-            {munuList?.map((menu) => {
-              return (
-                <DropdownButton
-                  key={menu.name}
-                  color="transPrimary"
-                  onClick={(event) => {
-                    event.stopPropagation();
+            </NavLink>
+          </Typography>
+        </Flex>
+        <Flex
+          paddingX={1}
+          paddingBottom={1}
+          alignItems="flex-start"
+          flexDirection={'column'}
+          justifyContent={'space-between'}
+        >
+          {munuList?.map((menu) => {
+            return (
+              <DropdownButton
+                key={menu.name}
+                color="transPrimary"
+                onClick={(event) => {
+                  event.stopPropagation();
+                  setTimeout(() => {
                     setShow(false);
-                    menu.onClick();
-                  }}
-                >
-                  {menu.icon}
-                  <Typography variant="subtitle1" fontWeight={700}>
-                    {menu.name}
-                  </Typography>
-                </DropdownButton>
-              );
-            })}
-          </Flex>
-        </Drawer>
-      }
+                  }, 100);
+                  menu.onClick();
+                }}
+              >
+                {menu.icon}
+                <Typography variant="subtitle1" fontWeight={700}>
+                  {menu.name}
+                </Typography>
+              </DropdownButton>
+            );
+          })}
+        </Flex>
+      </Drawer>
     </div>
   );
 }
